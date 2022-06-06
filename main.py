2c57b12489aa5a156d734fcc822301a9cb7d34b2
nmautz <nmautz@zagmail.gonzaga.edu>
2022-06-06 11:40:12 -0700

    properly get cookies and performs search

--- main.py
+++ main.py
@@ -1,18 +1,34 @@
 import requests
 import json
-
-# =-----------------------------------=
+from selenium import webdriver
+from selenium.webdriver.chrome.options import ChromiumOptions
+from selenium.webdriver.common.by import By
 import requests
+import time
 
-session = requests.Session()
+# Get Cookies
+options = ChromiumOptions()
+options.add_argument("--headless")
+driver = webdriver.Chrome(options=options)
 
-response = session.get("https://xe.gonzaga.edu/StudentRegistrationSsb/ssb/searchResults/searchResults")
-cookies = session.cookies.get_dict()
-session.close()
+driver.get("https://xe.gonzaga.edu/StudentRegistrationSsb/ssb/registration")
+driver.find_element(By.ID, "classSearchLink").click()
+driver.find_element(By.ID, "s2id_txt_term").click()
+time.sleep(1)
+driver.find_element(By.ID, "202310").click()
+driver.find_element(By.ID, "term-go").click()
 
-session_cookie = "JSESSIONID=" + str(cookies['JSESSIONID'])
-oracle_cookie = "X-Oracle-BMC-LBS-Route=" + str(cookies['X-Oracle-BMC-LBS-Route'])
+session_cookie = "JSESSIONID=" + driver.get_cookies()[1]["value"]
+oracle_cookie = "X-Oracle-BMC-LBS-Route=" + driver.get_cookies()[0]["value"]
 
+driver.quit()
+
+cookie_string = str(session_cookie) + "; " + str(oracle_cookie)
+
+# ____________________
+
+
+# prepare request
 url = "https://xe.gonzaga.edu/StudentRegistrationSsb/ssb/searchResults/searchResults"
 
 payload = ""
@@ -20,7 +36,7 @@ headers = {
     'Accept': "application/json, text/javascript, */*; q=0.01",
     'Accept-Language': "en-US,en;q=0.9",
     'Connection': "keep-alive",
-    'Cookie': str(session_cookie) + "; " + str(oracle_cookie),
+    'Cookie': cookie_string,
     'Referer': "https://xe.gonzaga.edu/StudentRegistrationSsb/ssb/classSearch/classSearch",
     'Sec-Fetch-Dest': "empty",
     'Sec-Fetch-Mode': "cors",
@@ -51,7 +67,6 @@ while i < 2300:
 
 print(session_cookie)
 print(oracle_cookie)
-
 
 for response in responses:
     json_response = json.loads(response)
