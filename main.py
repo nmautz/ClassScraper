bb87613ffd676c823310a3367e3bfb6dae4603b4
nmautz <nmautz@zagmail.gonzaga.edu>
2022-06-09 18:17:46 -0700

    working

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ headers = {
 
 i = 0
 
-page_size = 400  # max is 499 ish
+page_size = 400
 
 responses = []
 
@@ -63,14 +63,119 @@ while i < 2300:
     i += page_size
     responses.append(response.text)
 
-
 # Parse responses
 
 
 print(session_cookie)
 print(oracle_cookie)
 
+class_list_dict = {"data": []}
+
 for response in responses:
     json_response = json.loads(response)
 
-    print(json_response["data"])
+    response_length = len(json_response["data"])
+
+    for i in range(0, response_length):
+
+        sectionAttributeCodes = []
+        sectionAttributeDescriptions = []
+        # Get Section attributes
+        attributeCount = len(json_response["data"][i]["sectionAttributes"])
+        for c in range(0, attributeCount):
+            sectionAttributeCodes.append(json_response["data"][i]["sectionAttributes"][c]["code"])
+            sectionAttributeDescriptions.append(json_response["data"][i]["sectionAttributes"][c]["description"])
+
+        # Edge case for faculty section sometimes missing
+
+        profName = ""
+        profEmail = ""
+        if len(json_response["data"][i]["faculty"]) != 0:
+            profName = json_response["data"][i]["faculty"][0]["displayName"]
+            profEmail = json_response["data"][i]["faculty"][0]["emailAddress"]
+
+        # Edge case for meetingsFaculty section sometimes missing
+        #Set variables to empty str
+        beginTime = ""
+        buildingDescription = ""
+        category = ""
+        creditHourSession = ""
+        endDate = ""
+        endTime = ""
+        hoursWeek = ""
+        room = ""
+        startDate = ""
+        sunday = ""
+        monday = ""
+        tuesday = ""
+        wednesday = ""
+        thursday = ""
+        friday = ""
+        saturday = ""
+        intrucMethDesc = ""
+
+        if len(json_response["data"][i]["meetingsFaculty"]) != 0:
+            beginTime = json_response["data"][i]["meetingsFaculty"][0]["meetingTime"]["beginTime"],
+            buildingDescription = json_response["data"][i]["meetingsFaculty"][0]["meetingTime"][
+                "buildingDescription"],
+            category = json_response["data"][i]["meetingsFaculty"][0]["meetingTime"]["category"],
+            creditHourSession = json_response["data"][i]["meetingsFaculty"][0]["meetingTime"]["creditHourSession"],
+            endDate = json_response["data"][i]["meetingsFaculty"][0]["meetingTime"]["endDate"],
+            endTime = json_response["data"][i]["meetingsFaculty"][0]["meetingTime"]["endTime"],
+            hoursWeek = json_response["data"][i]["meetingsFaculty"][0]["meetingTime"]["hoursWeek"],
+            room = json_response["data"][i]["meetingsFaculty"][0]["meetingTime"]["room"],
+            startDate = json_response["data"][i]["meetingsFaculty"][0]["meetingTime"]["startDate"],
+            sunday = json_response["data"][i]["meetingsFaculty"][0]["meetingTime"]["sunday"],
+            monday = json_response["data"][i]["meetingsFaculty"][0]["meetingTime"]["monday"],
+            tuesday = json_response["data"][i]["meetingsFaculty"][0]["meetingTime"]["tuesday"],
+            wednesday = json_response["data"][i]["meetingsFaculty"][0]["meetingTime"]["wednesday"],
+            thursday = json_response["data"][i]["meetingsFaculty"][0]["meetingTime"]["thursday"],
+            friday = json_response["data"][i]["meetingsFaculty"][0]["meetingTime"]["friday"],
+            saturday = json_response["data"][i]["meetingsFaculty"][0]["meetingTime"]["saturday"],
+
+
+
+        class_list_dict["data"].append(
+
+            {
+                "subject": json_response["data"][i]["subject"],
+                "subjectDescription": json_response["data"][i]["subjectDescription"],
+                "courseNumber": json_response["data"][i]["courseNumber"],
+                "scheduleTypeDescription": json_response["data"][i]["scheduleTypeDescription"],
+                "courseTitle": json_response["data"][i]["courseTitle"],
+                "maximumEnrollment": json_response["data"][i]["maximumEnrollment"],
+                "enrollment": json_response["data"][i]["enrollment"],
+                "seatsAvailable": json_response["data"][i]["seatsAvailable"],
+                "waitCapacity": json_response["data"][i]["waitCapacity"],
+                "waitCount": json_response["data"][i]["waitCount"],
+                "waitAvailable": json_response["data"][i]["waitAvailable"],
+                "professorName": profName,
+                "professorEmail": profEmail,
+                "beginTime": beginTime,
+                "buildingDescription": buildingDescription,
+                "category": category,
+                "creditHourSession": creditHourSession,
+                "endDate": endDate,
+                "endTime": endTime,
+                "hoursWeek": hoursWeek,
+                "room": room,
+                "startDate": startDate,
+                "sunday": sunday,
+                "monday": monday,
+                "tuesday": tuesday,
+                "wednesday": wednesday,
+                "thursday": thursday,
+                "friday": friday,
+                "saturday": saturday,
+                "instructionalMethodDescription": json_response["data"][i]["instructionalMethodDescription"],
+                "attributeCodes": sectionAttributeCodes,
+                "attributeDesc": sectionAttributeDescriptions
+
+            }
+        )
+
+print("Writing result to file classes.json")
+f = open("classes.json", "w")
+f.write(json.dumps(class_list_dict))
+f.close()
+print("Finished!")
